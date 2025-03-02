79debcec98187a1f583d5e3b1f36534301f8f034
marekpno <marekpno@gmail.com>
2025-03-02 22:08:49 +0100

    django blog  #15 minuta 8

---
+++ blog/myblog/form.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Post, Category
+
+#cat_name = [('kosz', 'siata'), ('fiza', 'mata'), ('szachy', 'gra')]
+cat_name = Category.objects.all().values_list('name','name')
+
+cat_name_list =[]
+
+for item in cat_name:
+    cat_name_list.append(item)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=cat_name_list, attrs={'class': 'form=control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control'}),
+        }
---
+++ blog/myblog/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+#from . import views
+from .views import HomeView, AddPostView, ArticleDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+
+urlpatterns = [
+    #path('',views.home, name = 'home'),
+    path('',HomeView.as_view(), name="home"),
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name = 'add_post'),
+    path('category/', AddCategoryView.as_view(), name = 'add_category'),
+    path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
+    path('category/<str:cats>/',CategoryView, name = 'category'),
+    #path('category/<slug:slug>/', CategoryView, name='category'),
+
+
+]
