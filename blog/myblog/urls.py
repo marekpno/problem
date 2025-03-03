10627ab5207c3c49e4e9782997fe82bcb573d6b9
marekpno <marekpno@gmail.com>
2025-03-03 21:49:14 +0100

    django blog  #15 zakonczony

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 #from . import views
-from .views import HomeView, AddPostView, ArticleDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView
+from .views import HomeView, AddPostView, ArticleDetailView, UpdatePostView, DeletePostView, AddCategoryView, CategoryView, CategoryListView
 
 urlpatterns = [
     #path('',views.home, name = 'home'),
@@ -11,7 +11,7 @@ urlpatterns = [
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name = 'update_post'),
     path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
     path('category/<str:cats>/',CategoryView, name = 'category'),
-    #path('category/<slug:slug>/', CategoryView, name='category'),
+    path('category-list/',CategoryListView, name = 'category-list'),
 
 
 ]
